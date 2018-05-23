e13d3195a144cae988397efa93895b34c96ddb41
Kevin VIGNAU <kvignau@e2r4p15.42.fr>
2018-05-23 11:45:44 +0200

    ft_puts secure NULL value

--- ft_puts.s
+++ ft_puts.s
@@ -5,30 +5,51 @@
 section .data
 endl:
 	.string db 10
+	.null db "(null)", 10
+	.len equ $ - endl.null
 
 section .text
 	global _ft_puts
 
 _ft_puts:
 	mov rax, 0
+	xor rsi, rsi
+	xor rdx, rdx
 	mov rsi, rdi
 	mov rdi, STDOUT
+	cmp rsi, 0x0
+	je exit
 	jmp choice
 
 choice:
-	mov al, [rsi + rdx] ; [rsi + rdx] == *(rsi + rdx)
+	mov al, [rsi + rdx]
 	cmp al, 0x0
 	jne plus
-	lea rsi, [rel rsi]	; lea rsi, [rel rsi] == rsi = &rsi
+	cmp rdx, 0
+	je return
+	lea rsi, [rel rsi]
 	mov rax, MACH_SYSCALL(WRITE)
 	syscall
+	jmp return
+
+plus:
+	add rdx, 1
+	jmp choice
+
+return:
 	mov rax, MACH_SYSCALL(WRITE)
 	mov	rdi, 1
 	lea rsi, [rel endl.string]
 	mov rdx, 1
 	syscall
+	mov rax, 10
 	ret
 
-plus:
-	add rdx, 1
-	jmp choice
+exit:
+	xor rsi, rsi
+	mov rax, MACH_SYSCALL(WRITE)
+	lea rsi, [rel endl.null]
+	mov rdx, endl.len
+	syscall
+	mov rax, 10
+	ret
